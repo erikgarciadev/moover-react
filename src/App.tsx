01082ce9aad45d6c91acb5dd22d7ca9d77c3fb43
Erik Garcia Tacas <erik.fisi16@gmail.com>
2022-02-09 00:55:45 -0500

    feat: add page home

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,25 @@
-import React from 'react';
-import { Routes, Route, Navigate } from 'react-router-dom';
-import Header from './components/header';
-import Contact from './pages/contact';
-import OrderPackage from './pages/orderPackage';
-import Home from './pages/home';
-import Package from './pages/package';
+import React from "react";
+import { Routes, Route, Navigate } from "react-router-dom";
+import Header from "./components/header";
+import Contact from "./pages/contact";
+import OrderPackage from "./pages/orderPackage";
+import Home from "./pages/home";
+import Package from "./pages/package";
+import { Box } from "@chakra-ui/react";
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <Routes>
-        <Route path='' element={<Home />} />
-        <Route path='/contact' element={<Contact />} />
-        <Route path='/order-package' element={<OrderPackage />} />
-        <Route path='/package/:orderId' element={<Package />} />
-        <Route path="*" element={<Navigate to='/' />} />
-      </Routes>
+      <Box h='calc(100vh - 80px)'>
+        <Routes>
+          <Route path="" element={<Home />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/order-package" element={<OrderPackage />} />
+          <Route path="/package/:orderId" element={<Package />} />
+          <Route path="*" element={<Navigate to="/" />} />
+        </Routes>
+      </Box>
     </div>
   );
 }
--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,5 +1,59 @@
-import React from 'react';
+import { Box, Image } from "@chakra-ui/react";
+import logo from "../../assets/logo.svg";
+import React from "react";
 
 export default function Home() {
-  return <div>Home</div>;
+  return (
+    <Box
+      h="full"
+      display="flex"
+      flexDirection={{
+        base: "column-reverse",
+        md: "row",
+      }}
+    >
+      <BoxCustom>
+        <Image
+          w="80%"
+          objectFit="cover"
+          src={logo}
+          alt="logo"
+          borderRadius={50}
+        />
+      </BoxCustom>
+      <BoxCustom>
+        <Box
+          as="p"
+          textAlign="center"
+          fontWeight="bold"
+          fontSize={{
+            base:'24px',
+            md:'36px'
+          }}
+          color="#77b6ea"
+        >
+          Realizamos servicio de traslado de paquetes de manera segura y
+          sanitizada
+        </Box>
+      </BoxCustom>
+    </Box>
+  );
+}
+
+function BoxCustom({ children }: { children: React.ReactNode }) {
+  return (
+    <Box
+      h="full"
+      px="1em"
+      w={{
+        base: "100%",
+        md: "50%",
+      }}
+      display="flex"
+      justifyContent="center"
+      alignItems="center"
+    >
+      {children}
+    </Box>
+  );
 }
